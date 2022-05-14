8ced7404479deb14f53362fac0e1c70baccaf718
Ruiz-Martinez-Andriks-Imanol <105221206+Ruiz-Martinez-Andriks-Imanol@users.noreply.github.com>
2022-05-14 16:39:24 -0500

    prog_est_prog11
    Escribir el número de mes y que el programa lo imprima, devolviendo también los valores erróneos.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # prog_est_prog11
-Programa que escribe el mes usando switch
+Autor : Andriks Imanol Ruiz Martínez, Fecha: 17/02/2022
+Escuela: Universidad del Valle de Mexico Campus Villahermosa <br>
+Materia: Programación Estructrada <br>
+Ciclo: 01/2022</p>
+<br>
+<p>Este es un programa de casos usando switch en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+  <ul>
+    <li>Variables enteras</li>
+    <li>printf para mostrar varias variables</li>
+    <li>scanf</li>
+    <li>El uso de include para las librerias</li>
+    <li>Switch</li>
+    <li>Comentarios para la doumentación interna del programa.</li>
+    </ul>
+    </p>
+<br>
+Objetivo: Escribir el numero del mes y que me escriba el mes que corresponde. Deberá escribir los valores erróneos.
+<br>
+<p>Breve descripción:
+Este programa escribe el mes que quiero y que lo imprima, usando “switch” que permite realizar caso (case) que nos servirá de reemplazo del if. Para cada caso, se usa un “break” para poder separarlo de cada uno y al terminar, escribimos un “default” para indicar al programa lo que se tiene que hacer si se introduce algo más que un número o el límite de número permitido.
+<br>
+</p>
+
---
+++ prog11.cpp
@@ -0,0 +1,50 @@
+/* Autor: Andriks Imanol Ruiz Mártinez, Realizado: 17/02/2022
+Escuela: Universidad del Valle de Mexico Campus Villahermosa <br>
+Materia: Programación Estructrada <br>
+Ciclo: 01/2022</p>
+<br>
+<p>Este es un programa de casos usando switch con operadores lógicos en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+    *Variables enteras
+    -printf para mostrar varias variables
+    -scanf
+    -El uso de include para las libreria.
+    -Switch
+    -Comentarios para la doumentación interna del programa.
+*/
+#include<stdio.h>
+int main() {
+	//Declaración
+	int mes;
+	//Proceso
+	printf("Introduce el numero de mes: ");
+	scanf("%d", &mes);
+	switch(mes){ //Switch
+		case 1: printf("Enero");
+				break;
+		case 2: printf("Febrero");
+				break;
+		case 3: printf("Marzo");
+				break;
+		case 4: printf("Abril");
+				break;
+		case 5: printf("Mayo");
+				break;
+		case 6: printf("Junio");
+				break;
+		case 7: printf("Julio");
+				break;
+		case 8: printf("Agosto");
+				break;
+		case 9: printf("Septiembre");
+				break;
+		case 10: printf("Octubre");
+				break;
+ 		case 11: printf("Noviembre");
+				break;
+		case 12: printf("Diciembre");
+				break;
+		default: printf("Numero de mes invalido");		
+	}
+	return 0;
+}
